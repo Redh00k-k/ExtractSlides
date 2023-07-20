0feab2b8e84243ef7c5942589060555c57e438da
Redh00k-k <jack.frost.6581@gmail.com>
2023-07-20 09:16:07 +0900

    [update] Refactoring

--- ExtractSlides.ps1
+++ ExtractSlides.ps1
@@ -6,13 +6,31 @@ Param(
 )
 
 function Extract-Slide {
-    $srcPPTX    = $args[0]
-    $dstPPTX    = $args[1]
+    $srcPath    = $args[0]
+    $dstPath    = $args[1]
     $words      = $args[2]
-    $numSlide = $srcPPTX.Slides.Count
+    try{
+        $powerPoint = New-Object -ComObject powerpoint.application
+        $powerPoint.Visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
 
-    # Uncomment code($dstPPTX_other) if you want to extract non-matching slides as well.
-    # $dstPPTX_other = $powerPoint.Presentations.Add()
+        $srcPPTX = $powerPoint.presentations.open($srcPath, $True, $False)
+        $dstPPTX = $powerPoint.Presentations.Add()
+
+        # Uncomment code($dstPPTX_other) if you want to extract non-matching slides as well.
+        # $dstPPTX_other = $powerPoint.Presentations.Add()
+
+        # Copy title(Slide 1) from srcPPTX
+        [void]$dstPPTX.Slides.InsertFromFile($srcPath, 0, 1, 1)
+    }
+    catch [System.IO.FileNotFoundException]{
+        Write-Output "Could not find $src"
+        exit
+    }
+    catch [System.IO.IOException]{
+        Write-Output "IO error with the file: $src or $dst"
+        exit
+    }
+    $numSlide = $srcPPTX.Slides.Count
 
     # Slide Loop
     for ($i=1; $i -le $numSlide; $i++){
@@ -38,35 +56,14 @@ function Extract-Slide {
 
     $dstPPTX.Saveas($dstPath)
     # $dstPPTX_other.Saveas($dstPath + "_other")
-}
-
-function Main {
-    try{
-        $powerPoint = New-Object -ComObject powerpoint.application
-        $powerPoint.Visible = [Microsoft.Office.Core.MsoTriState]::msoTrue
-
-        $srcPPTX = $powerPoint.presentations.open($srcPath, $True, $False)
-        $dstPPTX = $powerPoint.Presentations.Add()
-
-        # Copy title(Slide 1) from srcPPTX
-        [void]$dstPPTX.Slides.InsertFromFile($srcPath, 0, 1, 1)
-    }
-    catch [System.IO.FileNotFoundException]{
-        Write-Output "Could not find $src or $dst"
-        exit
-    }
-    catch [System.IO.IOException]{
-        Write-Output "IO error with the file: $src or $dst"
-        exit
-    }
-
-    Extract-Slide $srcPPTX $dstPPTX $words
 
     # Cleanup
     $srcPPTX.Close()
     $srcPPTX = $null
     $dstPPTX.Close()
     $dstPPTX = $null
+    # $dstPPTX_other.Close()
+    # $dstPPTX_other = $null
 
     $powerPoint.Quit()
     $powerPoint = $null
@@ -85,4 +82,4 @@ Write-Host "SrcFile: $src SrcFilePath: $srcPath"
 Write-Host "DstFile: $dst DstFilePath: $dstPath"
 Write-Host "Word List: $words"
 
-Main
+Extract-Slide $srcPath $dstPath $words
